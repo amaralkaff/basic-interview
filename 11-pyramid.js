e9c8cec844cbfa2cca5b56166f8fe82efa77101e
amaralkaff <tpamangly@gmail.com>
2025-01-31 04:34:40 +0800

    feat: add pyramids

---
+++ 11-pyramid.js
@@ -0,0 +1,17 @@
+function steps(n) {
+    const mid = Math.floor((2*n - 1)/2)
+    for (let row = 0; row < n; row++) {
+        let line = ''
+        for (let col = 0; col < 2 * n - 1; col++) {
+            if ( col >= mid - row && col <= mid + row ) {
+                line += '#'
+            }else {
+                line += ' '
+            }
+        }
+        console.log(line);
+        
+    }
+}
+
+steps(10)
