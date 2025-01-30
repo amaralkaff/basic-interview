47e56026bbc0128795deacc5d2c47c99339cc07a
amaralkaff <tpamangly@gmail.com>
2025-01-31 02:47:06 +0800

    feat: add anagrams sort solution

--- 7-anagrams.js
+++ 7-anagrams.js
@@ -1,21 +1,29 @@
-function charMap(str) {
-    const charMap = {}
-    str = str.toLowerCase().replace(/[\W]/g,"")
-    for (let char of str) {
-        charMap[char] = ++charMap[char] || 1
-    }
-    return charMap
+// function charMap(str) {
+//     const charMap = {}
+//     str = str.toLowerCase().replace(/[\W]/g,"")
+//     for (let char of str) {
+//         charMap[char] = ++charMap[char] || 1
+//     }
+//     return charMap
+// }
+
+// function anagrams(stringA, stringB) {
+//     const charMapA = charMap(stringA)
+//     const charMapB = charMap(stringB)
+//     if (Object.keys(charMapA).length !== Object.keys(charMapB).length) return false
+//     for (let key in charMapA) {
+//         if (charMapA[key] !== charMapB[key]) return false
+//     }
+
+//     return true
+// }
+
+
+function charmap(str) {
+    return str.toLowerCase().replace(/[\W]/g,'').split("").sort().join('')
 }
-
 function anagrams(stringA, stringB) {
-    const charMapA = charMap(stringA)
-    const charMapB = charMap(stringB)
-    if (Object.keys(charMapA).length !== Object.keys(charMapB).length) return false
-    for (let key in charMapA) {
-        if (charMapA[key] !== charMapB[key]) return false
-    }
-
-    return true
+    return charmap(stringA) === charmap(stringB)
 }
 
 console.log(anagrams("RAIL! SAFETY!", 'fairy tales'));
