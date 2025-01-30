27e210ad41c5d2320c41da2cb1411e16f1fa550d
amaralkaff <tpamangly@gmail.com>
2025-01-31 04:06:13 +0800

    feat: add steps string pattern

---
+++ 10-steps-string-pattern.js
@@ -0,0 +1,15 @@
+function steps(n) {
+    for (let row = 0; row < n; row++) {
+        let line = ' '
+        for (let col = 0; col < n; col++) {
+            if (col <= row) {
+                line += '#'
+            } else {
+                line += ' '
+            }
+        }
+        console.log(line);
+    }
+}
+
+steps(10)
